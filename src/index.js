cf1a2708e571c050a1c4a4431d3b281f9704c6fc
Yunfei Zhang <yunfeizhang@Nurse-Joy.local>
2019-04-17 19:00:57 -0700

    refactor data loading in componentDidMount

--- src/index.js
+++ src/index.js
@@ -5,16 +5,19 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = { lat: null, errorMessage: "" };
+  }
 
+  componentDidMount() {
     window.navigator.geolocation.getCurrentPosition(
-      position => {
-        this.setState({ lat: position.coords.latitude });
-      },
-      err => {
-        this.setState({ errorMessage: err.message });
-      }
+      position => this.setState({ lat: position.coords.latitude }),
+      err => this.setState({ errorMessage: err.message })
     );
   }
+
+  componentDidUpdate() {
+    console.log("My component updated");
+  }
+
   render() {
     if (this.state.errorMessage && !this.state.lat) {
       return <div>Error: {this.state.errorMessage}</div>;
