481cfe0f46b9cb2977aa35f4da4f5c7b964f7aeb
Yunfei Zhang <beethovenzhang@gmail.com>
2019-05-01 19:29:22 -0700

    add customized and default loading message

--- src/index.js
+++ src/index.js
@@ -25,7 +25,7 @@ class App extends React.Component {
     if (this.state.lat && !this.state.errorMessage) {
       return <SeasonDisplay lat={this.state.lat} />;
     }
-    return <Spinner />;
+    return <Spinner message="Please accept location request" />;
   }
 }
 
