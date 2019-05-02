6971584887946c4817e1f90060277ce4546827e4
Yunfei Zhang <beethovenzhang@gmail.com>
2019-05-01 18:17:40 -0700

    add a loading screen

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDom from "react-dom";
 import SeasonDisplay from "./SeasonDisplay";
+import Spinner from "./Spinner";
 
 class App extends React.Component {
   state = { lat: null, errorMessage: "" };
@@ -24,7 +25,7 @@ class App extends React.Component {
     if (this.state.lat && !this.state.errorMessage) {
       return <SeasonDisplay lat={this.state.lat} />;
     }
-    return <div> Loading! {this.state.lat}</div>;
+    return <Spinner />;
   }
 }
 
